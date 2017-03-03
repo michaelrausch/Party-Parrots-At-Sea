527afdb4a94a51fcefdf0e4fe27cc0a099cf5af3
Michael Rausch <me@michaelrausch.nz>
2017-03-03 16:22:31 +1300

    Created boat class & tests

---
+++ src/main/java/seng302/Boat.java
@@ -0,0 +1,32 @@
+package seng302;
+
+/*
+	Represents a boat in the race.
+
+	@param teamName The name of the team sailing the boat
+*/
+public class Boat
+{
+	// The name of the team, this is also the name of the boat
+	private String teamName = null;
+
+	public Boat(String teamName) {
+		this.teamName = teamName;
+	}
+
+	/*
+		Returns the name of the team sailing the boat
+		@returns The name of the team 
+	*/
+	public String getTeamName(){
+		return this.teamName;
+	}
+
+	/*
+		Sets the name of the team sailing the boat
+		@param teamName The name of the team
+	*/
+	public void setTeamName(String teamName){
+		this.teamName = teamName;
+	}
+}
