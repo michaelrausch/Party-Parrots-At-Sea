764aac4b7cce484ff1c5603d9571a4c68e715c35
Michael Rausch <mra106@uclive.ac.nz>
2017-03-06 12:07:21 +1300

    Added method to create the race with names from the config file
    - # implement

--- src/main/java/seng302/App.java
+++ src/main/java/seng302/App.java
@@ -1,19 +1,56 @@
 package seng302;
 
 import java.util.ArrayList;
+import java.lang.reflect.Array;
+import java.util.Random;
+import java.util.Collections;
+import java.util.List;
 
 public class App 
 {
+	public static Race createRace(int numberOfBoats) throws Exception{
+		Race race = new Race();
+
+		// Read team names from file
+		FileParser fp = new FileParser("src/test/java/seng302/valid.json");
+		ArrayList<String> boatNames = fp.getTeams();
+
+		// Shuffle team names
+		long seed = System.nanoTime();
+		Collections.shuffle(boatNames, new Random(seed));
+
+		if (numberOfBoats > Array.getLength(boatNames.toArray())){
+			return null;
+		}
+
+		for (int i = 0; i < numberOfBoats; i++) {
+			race.addBoat(new Boat(boatNames.get(i)));
+		}
+
+		return race;
+	}
+
     public static void main( String[] args )
     {
-    	Race race = new Race();
-    	race.addBoat(new Boat("Team 1"));
-    	race.addBoat(new Boat("Team 2"));
- 
- 		race.displayStartingBoats();
+    	Race race = null;
 
- 		System.out.println("");
+    	try{
+    		 race = createRace(2);
+    		
+    	}
+    	catch (Exception e){
+    		System.out.println(e);
+    	}
 
-    	race.displayFinishingOrder();
+    	if (race != null){
+    		race.displayStartingBoats();
+
+	 		System.out.println("");
+
+	    	race.displayFinishingOrder();	
+    	}
+    	else{
+    		System.out.println("e");
+    	}
     }
 }
