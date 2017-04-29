246083460ef2bc979c8b86ad9257b7521ba16235
Peter Galloway <ptg19@uclive.ac.nz>
2017-04-29 20:13:34 +1200

    partway through fixing boat movement to be updated from the data valid timestamp rather than the data sent timestamp #story[820]

---
+++ src/main/java/seng302/models/parsers/packets/BoatPositionPacket.java
@@ -0,0 +1,23 @@
+package seng302.models.parsers.packets;
+
+public class BoatPositionPacket {
+    private long boatId;
+    private long timeValid;
+    private double lat;
+    private double lon;
+    private double heading;
+    private double groundSpeed;
+
+    public BoatPositionPacket(long boatId, long timeValid, double lat, double lon, double heading, double groundSpeed) {
+        this.boatId = boatId;
+        this.timeValid = timeValid;
+        this.lat = lat;
+        this.lon = lon;
+        this.heading = heading;
+        this.groundSpeed = groundSpeed;
+    }
+
+    public long getTimeValid() {
+        return timeValid;
+    }
+}
