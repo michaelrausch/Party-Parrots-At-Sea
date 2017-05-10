8fa7829a3c13cb6581d0974dcdf1f4db09d3bfbe
Haoming Yin <hyi25@uclive.ac.nz>
2017-05-10 20:52:46 +1200

    Created a canvas map class to fetch map image from google
    - also added Bound class to encapsulate map boundary.
    - created TestMapView and its controller just for testing.
    
    #story[928]

--- src/main/java/seng302/App.java
+++ src/main/java/seng302/App.java
@@ -13,67 +13,68 @@ public class App extends Application
 {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/views/MainView.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("/views/MainView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/views/TestMapView.fxml"));
         primaryStage.setTitle("RaceVision");
         primaryStage.setScene(new Scene(root));
-        primaryStage.setMaximized(true);
+//        primaryStage.setMaximized(true);
 
         primaryStage.show();
-        primaryStage.setOnCloseRequest(e -> {
-            StreamParser.appClose();
-            StreamReceiver.noMoreBytes();
-            System.out.println("[CLIENT] Exiting program");
-            System.exit(0);
-        });
+//        primaryStage.setOnCloseRequest(e -> {
+//            StreamParser.appClose();
+//            StreamReceiver.noMoreBytes();
+//            System.out.println("[CLIENT] Exiting program");
+//            System.exit(0);
+//        });
 
     }
 
-    public static void main(String[] args) {
-        StreamReceiver sr = null;
-
-        new ServerThread("Racevision Test Server");
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        if (args.length == 1 && args[0].equals("-standalone")){
-            return;
-        }
-
-        if (args.length == 3 && args[0].equals("-server")){
-
-            sr = new StreamReceiver(args[1], Integer.valueOf(args[2]), "RaceStream");
-
-        } else if(args.length == 2 && args[0].equals("-server")){
-            switch (args[1]) {
-                case "internal":
-                    sr = new StreamReceiver("localhost", 4949, "RaceStream");
-                    break;
-                case "staffserver":
-                    sr = new StreamReceiver("csse-s302staff.canterbury.ac.nz", 4941, "RaceStream");
-                    break;
-                case "official":
-                    sr = new StreamReceiver("livedata.americascup.com", 4941, "RaceStream");
-                    break;
-            }
-        }
-        //Change the StreamReceiver in this else block to change the default data source.
-        else{
-            sr = new StreamReceiver("localhost", 4949, "RaceStream");
-        }
-
-        sr.start();
-        StreamParser streamParser = new StreamParser("StreamParser");
-        streamParser.start();
-
-        launch(args);
-
-
-
-    }
+//    public static void main(String[] args) {
+//        StreamReceiver sr = null;
+//
+//        new ServerThread("Racevision Test Server");
+//
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        if (args.length == 1 && args[0].equals("-standalone")){
+//            return;
+//        }
+//
+//        if (args.length == 3 && args[0].equals("-server")){
+//
+//            sr = new StreamReceiver(args[1], Integer.valueOf(args[2]), "RaceStream");
+//
+//        } else if(args.length == 2 && args[0].equals("-server")){
+//            switch (args[1]) {
+//                case "internal":
+//                    sr = new StreamReceiver("localhost", 4949, "RaceStream");
+//                    break;
+//                case "staffserver":
+//                    sr = new StreamReceiver("csse-s302staff.canterbury.ac.nz", 4941, "RaceStream");
+//                    break;
+//                case "official":
+//                    sr = new StreamReceiver("livedata.americascup.com", 4941, "RaceStream");
+//                    break;
+//            }
+//        }
+//        //Change the StreamReceiver in this else block to change the default data source.
+//        else{
+//            sr = new StreamReceiver("localhost", 4949, "RaceStream");
+//        }
+//
+//        sr.start();
+//        StreamParser streamParser = new StreamParser("StreamParser");
+//        streamParser.start();
+//
+//        launch(args);
+//
+//
+//
+//    }
 }
 
 
