7d160eaf545927dfb0aecbaca50f53d59c1011b7
Zhi You Tan <zyt10@uclive.ac.nz>
2017-05-17 13:16:00 +1200

    Removed unused imports from controller.java

--- src/main/java/seng302/controllers/Controller.java
+++ src/main/java/seng302/controllers/Controller.java
@@ -1,44 +1,30 @@
 package seng302.controllers;
 
-import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import seng302.models.Yacht;
-import seng302.models.parsers.StreamParser;
-import seng302.models.parsers.XMLParser;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class Controller implements Initializable {
+
     @FXML
     private AnchorPane contentPane;
 
-    private void setContentPane(String jfxUrl){
-        try{
+    private void setContentPane(String jfxUrl) {
+        try {
             contentPane.getChildren().removeAll();
             contentPane.getChildren().clear();
             contentPane.getStylesheets().add(getClass().getResource("/css/master.css").toString());
-            contentPane.getChildren().addAll((Pane) FXMLLoader.load(getClass().getResource(jfxUrl)));
-        }
-        catch(javafx.fxml.LoadException e){
+            contentPane.getChildren()
+                .addAll((Pane) FXMLLoader.load(getClass().getResource(jfxUrl)));
+        } catch (javafx.fxml.LoadException e) {
             System.err.println(e.getCause());
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             System.err.println(e);
         }
     }
