5adb7c3762156deaddb8e99cd5bfa4999e228929
Michael Rausch <michael@michaelrausch.net>
2017-05-19 20:57:40 +1200

    Fixed failing ColorsTest
    #story[923]

--- src/test/java/seng302/ColorsTest.java
+++ src/test/java/seng302/ColorsTest.java
@@ -8,14 +8,11 @@ import seng302.models.Colors;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by ryan_ on 16/03/2017.
- */
 public class ColorsTest {
 
     @Test
     public void testNextColor() {
-        Color expectedColors[] = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, Color.PURPLE};
+        Color expectedColors[] = {Color.RED, Color.PERU, Color.SEAGREEN, Color.GREEN, Color.BLUE, Color.PURPLE};
         for (int i = 0; i<6; i++)
         {
             Assert.assertEquals(expectedColors[i], Colors.getColor());
