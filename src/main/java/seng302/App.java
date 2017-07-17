102b5f3ca186f54bd07089cf066638203e86287c
Kusal Ekanayake <kre39@uclive.ac.nz>
2017-07-18 10:46:41 +1200

    Added a program icon that I snipped from the logo using photoshop.

--- src/main/java/seng302/App.java
+++ src/main/java/seng302/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import seng302.models.PolarTable;
 import seng302.models.stream.StreamParser;
@@ -21,6 +22,7 @@ public class App extends Application {
         primaryStage.setScene(new Scene(root, 1530, 960));
         primaryStage.setMaxWidth(1530);
         primaryStage.setMaxHeight(960);
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/PP.png")));
 //        primaryStage.setMaximized(true);
 
         primaryStage.show();
